b1346d29e282a81de5e6d72e3f69ca80a36c629d
gxd <guoxiaodong@haqu.com>
2020-06-21 11:23:44 +0800

    nothing

--- app/src/main/java/com/demo/customview/MainActivity.java
+++ app/src/main/java/com/demo/customview/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.demo.customview.pager.PagerActivity;
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/demo/customview/pager/PagerRecyclerView.java
@@ -0,0 +1,17 @@
+package com.demo.customview.pager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by guoxiaodong on 2020/6/21 11:18
+ */
+public class PagerRecyclerView extends RecyclerView {
+    public PagerRecyclerView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
